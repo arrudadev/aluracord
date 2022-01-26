bc941c461a6416727d7977fb9dcf9cf1b6228ac0
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-01-25 21:20:35 -0300

    style: add Home page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,166 @@
+import { ReactNode } from 'react';
+
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => <h1>Hello World!</h1>;
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+
+import appConfig from '../../config.json';
+
+type TitleProps = {
+  tag: string;
+  children: ReactNode;
+};
+
+const Title = ({ tag, children }: TitleProps) => {
+  const Tag: any = tag || 'h1';
+  return (
+    <>
+      <Tag>{children}</Tag>
+
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals['000']};
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+};
+
+const Home: NextPage = () => {
+  const username = 'arrudadev';
+
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary['500'],
+        // eslint-disable-next-line
+        // @ts-ignore
+        backgroundImage:
+          'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundBlendMode: 'multiply',
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          flexDirection: {
+            xs: 'column',
+            sm: 'row',
+          },
+          width: '100%',
+          maxWidth: '700px',
+          borderRadius: '5px',
+          padding: '32px',
+          margin: '16px',
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          backgroundColor: appConfig.theme.colors.neutrals['700'],
+        }}
+      >
+        {/* Formulário */}
+        <Box
+          as="form"
+          styleSheet={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: { xs: '100%', sm: '50%' },
+            textAlign: 'center',
+            marginBottom: '32px',
+          }}
+        >
+          <Title tag="h1">Boas vindas de volta!</Title>
+
+          <Text
+            variant="body3"
+            styleSheet={{
+              marginBottom: '32px',
+              color: appConfig.theme.colors.neutrals['300'],
+            }}
+          >
+            {appConfig.name}
+          </Text>
+
+          <TextField
+            fullWidth
+            // eslint-disable-next-line
+            // @ts-ignore
+            textFieldColors={{
+              neutral: {
+                textColor: appConfig.theme.colors.neutrals['200'],
+                mainColor: appConfig.theme.colors.neutrals['900'],
+                mainColorHighlight: appConfig.theme.colors.primary['500'],
+                backgroundColor: appConfig.theme.colors.neutrals['800'],
+              },
+            }}
+          />
+
+          <Button
+            type="submit"
+            label="Entrar"
+            fullWidth
+            buttonColors={{
+              contrastColor: appConfig.theme.colors.neutrals['000'],
+              mainColor: appConfig.theme.colors.primary['500'],
+              mainColorLight: appConfig.theme.colors.primary['400'],
+              mainColorStrong: appConfig.theme.colors.primary['600'],
+            }}
+          />
+        </Box>
+        {/* Formulário */}
+
+        {/* Photo Area */}
+        <Box
+          styleSheet={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            maxWidth: '200px',
+            padding: '16px',
+            backgroundColor: appConfig.theme.colors.neutrals['800'],
+            border: '1px solid',
+            borderColor: appConfig.theme.colors.neutrals['999'],
+            borderRadius: '10px',
+            flex: 1,
+            // eslint-disable-next-line
+            // @ts-ignore
+            minHeight: '240px',
+          }}
+        >
+          <Image
+            styleSheet={{
+              borderRadius: '50%',
+              marginBottom: '16px',
+            }}
+            src={`https://github.com/${username}.png`}
+            alt={username}
+          />
+
+          <Text
+            variant="body4"
+            styleSheet={{
+              color: appConfig.theme.colors.neutrals['200'],
+              backgroundColor: appConfig.theme.colors.neutrals['900'],
+              padding: '3px 10px',
+              borderRadius: '1000px',
+            }}
+          >
+            {username}
+          </Text>
+        </Box>
+        {/* Photo Area */}
+      </Box>
+    </Box>
+  );
+};
 
 export default Home;
